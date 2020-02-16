08d75237ee259b01ad671ae54e9ef04e72e6ce3e
Hogan <hogan@dcpt.online>
2020-02-16 19:18:05 +0800

    首页链接redux

---
+++ src/containers/Home/components/Discount/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Discount extends Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <div className="discount">
+        <a className="discount_header">
+          <span className="discount_title">超值特惠</span>
+          <span className="discount_more">更多优惠</span>
+          <span className="discount_arrow"></span>
+        </a>
+        <div className="discount_content">
+          {data.map((item, index) => {
+            return (
+              <a key={item.id} className="discount_item">
+                <div className="discount_itemPic">
+                  <img width="100%" height="100%" src={item.picture} />
+                </div>
+                <div className="discount_itemTitle">{item.shop}</div>
+                <div className="discount_itemPriceWrapper">
+                  <ins className="discount_itemCurrentPrice">
+                    {item.currentPrice}
+                  </ins>
+                  <del className="discount_itemOldPrice">{item.oldPrice}</del>
+                </div>
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Discount;
